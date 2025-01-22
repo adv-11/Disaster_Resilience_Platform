f1642f8884a11c1c9797a5ba75a1cd1ff75f502a
adv-11 <advaitss11@gmail.com>
2025-01-23 01:29:54 +0530

    TO FIX

--- views/sos.py
+++ views/sos.py
@@ -7,18 +7,23 @@ import folium
 from streamlit_folium import st_folium
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate({
-        "type": st.secrets["type"],
-        "project_id": st.secrets["project_id"],
-        "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"],
-        "client_email": st.secrets["client_email"],
-        "client_id": st.secrets["client_id"],
-        "auth_uri": st.secrets["auth_uri"],
-        "token_uri": st.secrets["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
-    })
+    cred = credentials.Certificate('firebase_credentials.json')
+
+
+    # cred = credentials.Certificate({
+    #     "type": st.secrets["type"],
+    #     "project_id": st.secrets["project_id"],
+    #     "private_key_id": st.secrets["private_key_id"],
+    #     "private_key": st.secrets["private_key"],
+    #     "client_email": st.secrets["client_email"],
+    #     "client_id": st.secrets["client_id"],
+    #     "auth_uri": st.secrets["auth_uri"],
+    #     "token_uri": st.secrets["token_uri"],
+    #     "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    #     "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+    # })
+
+    
     firebase_admin.initialize_app(cred, {"databaseURL": "https://disaster-resilience-sos123-default-rtdb.firebaseio.com/"})
 
 # Function to fetch SOS messages from Firebase Realtime Database
